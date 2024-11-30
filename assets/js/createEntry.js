876a0f7650dfc41d77bcdf95c562a11195caf055
ShawnP-203 <sjplackiyil@stu.naperville203.org>
2024-11-30 16:28:45 -0600

    editing website style

--- assets/js/createEntry.js
+++ assets/js/createEntry.js
@@ -3,15 +3,9 @@ submitButton.addEventListener("click", async() => {
     // Get values from the entry form
     const date = document.querySelector("input.date").value;
 
-    // Find the radio button that is checked
-    const homButtons = document.querySelectorAll("input.habits:checked");
+    const habitOfMind = document.getElementById("hom").value;
 
-    // Equivalent to an if statement homButtons.length > 0 is the condition
-    // habitOfMind will be set to the selected button value if the condition is true, null is the condition is false
-    const habitOfMind =
-        homButtons.length > 0 ? homButtons[0].value : null;
-
-    const content = document.querySelector("textarea.content").value;
+    const content = document.getElementById("content").value;
     const entry = {date, habit: habitOfMind, content};
 
     // Send a POST request to the router and wait for a response
